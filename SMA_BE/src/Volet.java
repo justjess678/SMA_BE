350f79b1e526df83e75d040174533a5afc29ba20
Jessica Chambers <jess.chambers@gmail.com>
2018-11-14 23:33:56 +0100

    added unimplemented methods

--- SMA_BE/src/Volet.java
+++ SMA_BE/src/Volet.java
@@ -1,11 +1,13 @@
 public class Volet extends SourceLumiere {
-	// take into account the level of sunlight
-	private float lumSoleil;
 
-	public Volet() {
+	public Volet(Salle amas) {
+		super(amas);
 		this.luminosite = 100.0f;
 		this.lumSoleil = 0.0f;
 	}
+
+	// take into account the level of sunlight
+	private float lumSoleil;
 
 	@Override
 	public void augmenterLuminosite() {
@@ -39,6 +41,36 @@ public class Volet extends SourceLumiere {
 
 	public void setLumSoleil(float lumSoleil) {
 		this.lumSoleil = lumSoleil;
+	}
+
+	@Override
+	protected void onInitialization() {
+		this.luminosite = 100.0f;
+		this.lumSoleil = 0.0f;		
+	}
+
+	@Override
+	protected void onReady() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected double computeCriticality() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void onPerceive() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onDecideAndAct() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
