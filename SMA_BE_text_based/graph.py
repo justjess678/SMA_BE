24366d7346ffef2524a3c8d36bd3c1538a6bbc5e
Jessica Chambers <jess.chambers@gmail.com>
2018-11-22 21:42:05 +0100

    graphs now exported to .png

--- SMA_BE_text_based/graph.py
+++ SMA_BE_text_based/graph.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 21 22:19:34 2018
@@ -15,6 +16,7 @@ ax1.plot(x, y)
 ax1.set_title("Brightness")
 ax1.set_xlabel("Time")
 ax1.set_ylabel("Brightness (%)")
+fig1.savefig("brightness_graph.png")
 
 H = np.loadtxt("energy.txt")
 x, y = np.hsplit(H,2)
@@ -23,3 +25,4 @@ ax2.plot(x, y)
 ax2.set_title("Energy Consumption")
 ax2.set_xlabel("Time")
 ax2.set_ylabel("Energy consumed (Watts)")
+fig2.savefig("energy_graph.png")
